81bc6b889ff38e0c20df89164d3971051690b513
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-08-15 22:36:50 +0530

    Make Home and Complete Assignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,39 @@
-import Image from "next/image";
+"use client";
+import { AuroraBackground } from "@/components/ui/aurora-background";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <div>
+      <AuroraBackground>
+        <motion.div
+          initial={{ opacity: 0.0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="relative flex flex-col gap-4 items-center justify-center px-4"
+        >
+          <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+            Effortless and Secure Authentication in Next.js
+          </div>
+          <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
+            Next.js simplifies building secure authentication systems with its
+            API routes and dynamic features. Easily integrate authentication
+            methods like JWT or OAuth to protect user data and ensure a smooth
+            user experience.
+          </div>
+          <Link
+            href="/profile"
+            className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2"
+          >
+            Visit Profile
+          </Link>
+        </motion.div>
+      </AuroraBackground>
+    </div>
   );
 }
